a0a243a42cdd292ed4407a18fe4890392909f2c1
v-AMusayelyan <v-amusayelyan@quantumftg.com>
2024-07-30 20:20:19 +0400

    Initial Commit

---
+++ ShapeMorphingEffect/ShapeMorphingEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeMorphingEffectApp.swift
+//  ShapeMorphingEffect
+//
+//  Created by Solicy Ios on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ShapeMorphingEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
